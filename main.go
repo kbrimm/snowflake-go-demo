629f733cdf8546e263b2993ab8f48ee6d376abfc
Katy Brimm <katy.brimm@gmail.com>
2018-12-17 14:26:37 -0800

    Connects to Snowflake DB using values stored in parameters.json. Borrows REALLY heavily from the select1 test shipped with gosnoflake project.

--- main.go
+++ main.go
@@ -1,10 +1,85 @@
 package main
 
 import (
-    "fmt"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	sf "github.com/snowflakedb/gosnowflake"
 )
 
-func main() {
-    fmt.Println("snowflake-go-demo")
+// Parameters holds the contents of parameters.json
+type Parameters struct {
+	Account   string `json:"account"`
+	User      string `json:"user"`
+	Password  string `json:"password"`
+	Warehouse string `json:"warehouse"`
+	Database  string `json:"database"`
+	Schema    string `json:"schema"`
 }
 
+// getDSN constructs a DSN based on the test connection parameters
+func getDSN() (string, *sf.Config, error) {
+	paramConfig, err := os.Open("parameters.json")
+	byteStream, _ := ioutil.ReadAll(paramConfig)
+	var params Parameters
+	json.Unmarshal(byteStream, &params)
+
+	if err != nil {
+		panic("unable to read parameters.json")
+	}
+
+	cfg := &sf.Config{
+		Account:   params.Account,
+		User:      params.User,
+		Password:  params.Password,
+		Warehouse: params.Warehouse,
+		Database:  params.Database,
+		Schema:    params.Schema,
+	}
+
+	dsn, err := sf.DSN(cfg)
+	return dsn, cfg, err
+}
+
+func selectOne() {
+	dsn, cfg, err := getDSN()
+	if err != nil {
+		log.Fatalf("failed to create DSN from Config: %v, err: %v", cfg, err)
+	}
+
+	db, err := sql.Open("snowflake", dsn)
+	if err != nil {
+		log.Fatalf("failed to connect. %v, err: %v", dsn, err)
+	}
+	defer db.Close()
+	query := "SELECT 1 FROM TEST"
+	rows, err := db.Query(query) // no cancel is allowed
+	if err != nil {
+		log.Fatalf("failed to run a query. %v, err: %v", query, err)
+	}
+	defer rows.Close()
+	var v int
+	for rows.Next() {
+		err := rows.Scan(&v)
+		if err != nil {
+			log.Fatalf("failed to get result. err: %v", err)
+		}
+		if v != 1 {
+			log.Fatalf("failed to get 1. got: %v", v)
+		}
+	}
+	if rows.Err() != nil {
+		fmt.Printf("ERROR: %v\n", rows.Err())
+		return
+	}
+	fmt.Printf("Congrats! You have successfully run %v with Snowflake DB!\n", query)
+}
+
+func main() {
+	fmt.Println("Running snowflake-go-demo")
+	selectOne()
+}
